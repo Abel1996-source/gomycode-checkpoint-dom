eba0050fd51741ebd600b93b20c179d4bd7dcb11
Abel1996-source <98103644+Abel1996-source@users.noreply.github.com>
2022-02-20 12:16:15 +0000

    projet final de panier à Gomycode

--- js/script.js
+++ js/script.js
@@ -1,61 +1,162 @@
-let effect = document.getElementById("qty1")
-let effect1 = document.getElementById("qty2")
-let qtyminus = document.getElementById("minus-1")
-let qtyplus = document.getElementById("plus-1")
-let qtyplus1 = document.getElementById("plus-2")
-let qtyminus1 = document.getElementById("minus-2")
-qtyminus.addEventListener("click", decrementation)
-qtyminus1.addEventListener("click", decrementation1)
-qtyplus.addEventListener("click", incrementation)
-qtyplus1.addEventListener("click", incrementation1)
-    // gestion des prix
+let btnMinusAll = document.querySelectorAll("#btn-minus")
+let btnPlusAll = document.querySelectorAll("#btn-plus")
 
-let prix = document.getElementById("price-U")
-let prixT = document.getElementById("price-T")
-prixT.innerHTML = prix.innerHTML
+prixTotal()
 
-let prix1 = document.getElementById("prix-U")
-let prixT1 = document.getElementById("prix-T")
-prixT1.innerHTML = prix1.innerHTML
+btnPlusAll.forEach((btn) => {
+    btn.addEventListener("click", incrementQuantity)
 
-// prix total du parnier
-
-let total = document.getElementById("prix-total")
-total.innerHTML = parseInt(prixT.innerHTML) + parseInt(prixT1.innerHTML)
-
-
-
-function decrementation() {
-    if (!isNaN(parseInt(effect.value)) && parseInt(effect.value) > 1) {
-        effect.value = parseInt(effect.value) - 1
-        prixT1.innerHTML = parseInt(effect.value) * parseInt(prix1.innerHTML)
-        total.innerHTML = parseInt(prixT.innerHTML) + parseInt(prixT1.innerHTML)
-
-    } else {
-        return false;
+    function incrementQuantity() {
+        this.previousElementSibling.value = parseInt(this.previousElementSibling.value) + 1
+        sousTotal(this)
+        prixTotal()
     }
 
-}
 
-function decrementation1() {
-    if (!isNaN(parseInt(effect1.value)) && parseInt(effect1.value) > 1) {
-        effect1.value = parseInt(effect1.value) - 1
-        prixT.innerHTML = parseInt(effect1.value) * parseInt(prix.innerHTML)
-        total.innerHTML = parseInt(prixT.innerHTML) + parseInt(prixT1.innerHTML)
-    } else {
-        return false;
+
+});
+btnMinusAll.forEach((btn) => {
+    btn.addEventListener("click", decrementQuantity)
+
+    function decrementQuantity() {
+        if (this.nextElementSibling.value > 1) {
+            this.nextElementSibling.value = parseInt(this.nextElementSibling.value) - 1
+            sousTotal(this)
+            prixTotal()
+        }
+
     }
 
+
+});
+
+
+
+
+function sousTotal(elt) {
+    let btnPlusAll = document.querySelector("#btn-plus")
+    if (elt == btnPlusAll) {
+        let price = elt.parentElement.parentElement.nextElementSibling.children[0]
+        let qty = elt.previousElementSibling.value
+        let sousT = elt.parentElement.parentElement.nextElementSibling.nextElementSibling.children[0].children[0]
+        sousT.innerHTML = parseInt(qty) * parseInt(price.innerHTML) + ' Fcfa'
+    } else {
+        let price = elt.parentElement.parentElement.nextElementSibling.children[0]
+        let qty = elt.nextElementSibling.value
+        let sousT = elt.parentElement.parentElement.nextElementSibling.nextElementSibling.children[0].children[0]
+        sousT.innerHTML = parseInt(qty) * parseInt(price.innerHTML) + ' Fcfa'
+    }
+
+
 }
 
-function incrementation() {
-    effect.value = parseInt(effect.value) + 1
-    prixT1.innerHTML = parseInt(effect.value) * parseInt(prix1.innerHTML)
-    total.innerHTML = parseInt(prixT.innerHTML) + parseInt(prixT1.innerHTML)
+
+
+
+// Ajout de panier
+
+let name = document.querySelector(".name")
+let prix = document.querySelector(".prix")
+let addArticle = document.querySelector("#ajouter")
+
+addArticle.addEventListener("click", addProduct)
+
+function addProduct() {
+    let name = document.querySelector(".name").value
+    let price = document.querySelector(".prix").value
+
+    if (name != "" && price != "") {
+        document.querySelector("#all-produits").innerHTML += `  <tr>
+
+    <td>
+        <div class="produits">
+            <img id="img-1" src="res/images/image-product-1-thumbnail.jpg" alt="">
+            <div>
+                <h5 id="article1">${name} </h5>
+                <button class="remove">Suprimer</button>
+            </div>
+        </div>
+    </td>
+    <td>
+        <div class="quantity">
+            <button id="btn-minus"><img src="res/images/icon-minus.svg" alt=""></button>
+            <input type="text" class="qty" value="1">
+            <button id="btn-plus"><img src="res/images/icon-plus.svg" alt=""></button>
+        </div>
+    </td>
+
+    <td>
+        <h3 class="price">${price} Fcfa </h3>
+    </td>
+    <td>
+        <div class="sousT">
+            <p class="totalPrice">${price} Fcfa</p>
+        </div>
+    </td>
+
+</tr>`
+    }
+    let btnMinusAll = document.querySelectorAll("#btn-minus")
+    let btnPlusAll = document.querySelectorAll("#btn-plus")
+
+    btnPlusAll.forEach((btn) => {
+        btn.addEventListener("click", incrementQuantity)
+
+
+    });
+    btnMinusAll.forEach((btn) => {
+        btn.addEventListener("click", decrementQuantity)
+
+    });
+
+
+    function decrementQuantity() {
+        if (this.nextElementSibling.value > 1) {
+            this.nextElementSibling.value = parseInt(this.nextElementSibling.value) - 1
+            sousTotal(this)
+            prixTotal()
+        }
+
+    }
+
+
+    function incrementQuantity() {
+        this.previousElementSibling.value = parseInt(this.previousElementSibling.value) + 1
+            // sousTotal(this)
+        let price = this.parentElement.parentElement.nextElementSibling.children[0]
+        let qty = this.previousElementSibling.value
+        let sousT = this.parentElement.parentElement.nextElementSibling.nextElementSibling.children[0].children[0]
+        sousT.innerHTML = parseInt(qty) * parseInt(price.innerHTML) + ' Fcfa'
+        prixTotal()
+    }
+    prixTotal()
+    let elts = document.querySelectorAll(".remove")
+    elts.forEach((elt) => {
+        elt.addEventListener("click", removeProduct)
+
+    })
+
+
 }
 
-function incrementation1() {
-    effect1.value = parseInt(effect1.value) + 1
-    prixT.innerHTML = parseInt(effect1.value) * parseInt(prix.innerHTML)
-    total.innerHTML = parseInt(prixT.innerHTML) + parseInt(prixT1.innerHTML)
+function prixTotal() {
+    let sum = 0;
+    let totalPrice = document.querySelectorAll(".totalPrice")
+    let prix = document.querySelector("#prix-total")
+    totalPrice.forEach((elt) => {
+        sum = sum + parseInt(elt.innerHTML)
+    })
+    console.log(prix.innerHTML = sum);
+
 }
+
+function removeProduct() {
+
+    this.parentElement.parentElement.parentElement.parentElement.innerHTML = ""
+    prixTotal()
+}
+let elts = document.querySelectorAll(".remove")
+elts.forEach((elt) => {
+    elt.addEventListener("click", removeProduct)
+
+})
